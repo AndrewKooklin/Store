6e750388b947043311e8b9ea14dbf8456bb8d653
AndrewKooklin <bolt999@yandex.ru>
2023-01-20 15:38:01 +0300

    Добавление книг в корзину

---
+++ presentation/Store.Web/SessionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Store.Web.Models;
+using System.IO;
+using System.Text;
+
+namespace Store.Web
+{
+    public static class SessionExtensions
+    {
+        private const string key = "Cart";
+
+        public static void Set(this ISession session, Cart value)
+        {
+            if (value == null)
+                return;
+
+            using(var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(value.Items.Count);
+
+                foreach(var item in value.Items)
+                {
+                    writer.Write(item.Key);
+                    writer.Write(item.Value);
+                }
+
+                writer.Write(value.Amount);
+
+                session.Set(key, stream.ToArray());
+            }
+        }
+        // Чтение корзины из сессии 
+        public static bool TryGetCart(this ISession session, out Cart value)
+        {
+            if(session.TryGetValue(key, out byte[] buffer))
+            {
+                using(var stream = new MemoryStream(buffer))
+                using(var reader = new BinaryReader(stream, Encoding.UTF8, true))
+                {
+                    value = new Cart();
+
+                    var length = reader.ReadInt32();
+                    for(int i = 0; i < length; i++)
+                    {
+                        var bookId = reader.ReadInt32();
+                        var count = reader.ReadInt32();
+
+                        value.Items.Add(bookId, count);
+                    }
+
+                    value.Amount = reader.ReadDecimal();
+                    //Если удалось прочитать данные из корзины то true
+                    return true;
+                }
+            }
+            // Если не удалось прочитать данные корзины , то false
+            value = null;
+            return false;
+        }
+    }
+}
