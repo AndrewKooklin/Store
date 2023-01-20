8588624860b8451fb80c4b98201642f2a60ffd92
AndrewKooklin <bolt999@yandex.ru>
2023-01-20 11:47:13 +0300

    Покупатель знакомится с описанием книги

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,9 +7,12 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D.Knuth", "Art of Programming"),
-            new Book(2, "ISBN 12312-31232", "M.Fowler",  "Refactoring"),
-            new Book(3, "ISBN 12312-31233", "B.Kernighan, D.Ritchie", "C Programming Language")
+            new Book(1, "ISBN 0201038013", "D. Knuth", "Art of Programming, Vol. 1",
+                    "This volume begins with programming concept", 7.19m),
+            new Book(2, "ISBN 0201485672", "M. Fowler",  "Refactoring",
+                    "As the application of object technology--particularly the Java", 12.45m),
+            new Book(3, "ISBN 0131101633", "B. W. Kernighan, D. M. Ritchie", "C Programming Language",
+                    "Known as the bible of C, this classic bestseller introduces", 14.98m)
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -20,9 +23,15 @@ namespace Store.Memory
 
         public Book[] GetAllByTitleOrAuthor(string query)
         {
-            return books.Where(book => book.Author.Contains(query) 
+
+            return books.Where(book => book.Author.Contains(query)
                                     || book.Title.Contains(query))
                         .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
--- presentation/Store.Web/Controllers/SearchController.cs
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -14,11 +14,12 @@ namespace Store.Web.Controllers
         {
             this.bookService = bookService;
         }
+        // /search/index?query=title
         public IActionResult Index(string query)
         {
             var books = bookService.GetAllByQuery(query);
 
-            return View(books);
+            return View("Index", books);
         }
     }
 }
