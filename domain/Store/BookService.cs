41a39eb550973d2de6d741b9d339114e2db95469
AndrewKooklin <bolt999@yandex.ru>
2023-01-22 15:08:45 +0300

    Add Order

--- domain/Store/BookService.cs
+++ domain/Store/BookService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -8,6 +9,20 @@ namespace Store
     public class BookService
     {
         private readonly IBookRepository bookRepository;
+        //private readonly DbConnectionFactory dbConnectionFactory;
+
+        //private readonly Func<IDbConnection> dbConnectionFactory;
+
+        //public BookService(Func<IDbConnection> dbConnectionFactory)
+        //{
+        //    this.dbConnectionFactory = dbConnectionFactory;
+        //}
+
+        //public BookService(DbConnectionFactory dbConnectionFactory)
+        //{
+        //    this.dbConnectionFactory = dbConnectionFactory;
+        //}
+
         public BookService(IBookRepository bookRepository)
         {
             this.bookRepository = bookRepository;
@@ -22,37 +37,56 @@ namespace Store
             }
         }
 
-        private Book[] GetAllByIsbn(string isbn)
-        {
-            using( var connection = new SqlConnection())
-            using(var command = connection.CreateCommand())
-            {
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT" +
-                    " Id, Title, Author, Isbn, Description, Price"+
-                    "FROM Books"+
-                    "WHERE Isbn = @Isbn";
-                command.Parameters.AddWithValue("@Isbn", isbn);
-                using (var reader = command.ExecuteReader())
-                {
-                    var result = new List<Book>();
+        //private Book[] GetAllByIsbn(string isbn)
+        //{
+        //    //var connectionString = Environment.GetEnvironmentVariable("STORE_CONNECTION_STRING");
+        //    //using( var connection = new SqlConnection(connectionString))
 
-                    while (reader.Read())
-                    {
-                        var id = reader.GetInt32(0);
-                        var title = reader.GetString(1);
-                        var author = reader.GetString(2);
-                        var _isbn = reader.GetString(3);
-                        var description = reader.GetString(4);
-                        var price = reader.GetDecimal(5);
+        //    using (var connection = dbConnectionFactory())//.Create())
+        //    using (var command = connection.CreateCommand())
+        //    {
+        //        command.CommandType = CommandType.Text;
+        //        command.CommandText = "SELECT" +
+        //            " Id, Title, Author, Isbn, Description, Price" +
+        //            "FROM Books" +
+        //            "WHERE Isbn = @Isbn";
+        //        command.Parameters.Add(isbn);
 
-                        result.Add(new Book(id, title, author, _isbn, description, price));
-                    }
+        //        using (var reader = command.ExecuteReader())
+        //        {
+        //            var result = new List<Book>();
 
-                    return result.ToArray();
-                }
+        //            while (reader.Read())
+        //            {
+        //                var id = reader.GetInt32(0);
+        //                var title = reader.GetString(1);
+        //                var author = reader.GetString(2);
+        //                var _isbn = reader.GetString(3);
+        //                var description = reader.GetString(4);
+        //                var price = reader.GetDecimal(5);
 
-            }
-        }
+        //                result.Add(new Book(id, title, author, _isbn, description, price));
+        //            }
+
+        //            return result.ToArray();
+        //        }
+
+        //    }
+        //}
     }
+
+    //public class DbConnectionFactory
+    //{
+    //    private readonly IServiceProvider serviceProvider;
+
+    //    public DbConnectionFactory(IServiceProvider serviceProvider)
+    //    {
+    //        this.serviceProvider = serviceProvider;
+    //    }
+
+    //    public IDbConnection Create()
+    //    {
+    //        return (IDbConnection)serviceProvider.GetService(typeof(IDbConnection));
+    //    }
+    //}
 }
--- presentation/Store.Web/Startup.cs
+++ presentation/Store.Web/Startup.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,8 +30,14 @@ namespace Store.Web
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
+
+            //services.AddTransient<IDbConnection, SqlConnection>();
             services.AddSingleton<IBookRepository, BookRepository>();
             services.AddSingleton<BookService>();
+
+            //services.AddScoped<IDbConnection, SqlConnection>();
+            //services.AddSingleton<Func<IDbConnection>>(serviceProvider =>
+            //                                            serviceProvider.GetService<IDbConnection>);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
